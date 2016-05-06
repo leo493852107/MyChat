b79327955b166d6093d7dbcc2f1fbaaa05d7437c
leo <leo493852107@163.com>
2016-05-06 16:16:14 +0800

    集成环形sdk && 修复Build Setting -> bitcode -> no 可以真机调试

---
+++ MyChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyChat
+//
+//  Created by leo on 5/6/16.
+//  Copyright © 2016 leo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Podfile
@@ -0,0 +1,13 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '8.0'
+
+target 'MyChat' do
+	pod 'AFNetworking', '~> 3.0'
+	pod 'HyphenateFullSDK'
+
+
+end
+
+
+
+
